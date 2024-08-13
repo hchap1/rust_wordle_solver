d00888683f82df2d47604dbf1140b455f81cc75d
Harrison Chapman <hchap1@hotmail.com>
2024-08-13 12:41:13 +1000

    Added struct - currently broken state. Need to add/read dictionary.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 mod regex_word_search;
 use regex_word_search::cull_to_pattern;
+use std::fs::read;
+
+struct WordleGame {
+    possibilities: Vec<String>,
+    confirmed: Vec<(char, usize)>,
+    present: Vec<(char, Vec<usize>)>,
+    absent: Vec<char>
+}
+
+impl WordleGame {
+    fn new() -> Self {
+        read("dictionary.txt")
 
 fn main() {
     let mut test: Vec<String> = vec![String::from("eggs"), String::from("balls")];
-    cull_to_pattern(&String::from("..gs"), &mut test);
+    match cull_to_pattern(&String::from("..gs"), &mut test) {
+        Ok(_) => {
+            for word in test {
+                println!("{word}");
+            }
+        }
+        Err(e) => {
+            println!("{:?}", e);
+        }
+    }
 }
